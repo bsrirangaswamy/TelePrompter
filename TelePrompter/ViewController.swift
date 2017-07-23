34b8607341255be4e90d798a103e4f17d396c063
Balakumaran Srirangaswamy <Bala@balakumarans-macbook-pro.local>
2017-07-23 16:15:10 -0500

    Initial Commit

---
+++ TelePrompter/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  TelePrompter
+//
+//  Created by Balakumaran Srirangaswamy on 7/23/17.
+//  Copyright © 2017 Bala. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
